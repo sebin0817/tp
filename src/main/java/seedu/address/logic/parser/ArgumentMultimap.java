ac95313e2468e4479ea38dfc676c3376c59e9363
Sebin Aum <78088740+sebin0817@users.noreply.github.com>
2024-03-15 17:44:17 +0800

    Update ArgumentMultimap.java

--- src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
+++ src/main/java/seedu/address/logic/parser/ArgumentMultimap.java
@@ -39,7 +39,7 @@ public class ArgumentMultimap {
      * Returns the last value of {@code prefix}.
      */
     public Optional<String> getValue(Prefix prefix) {
-        List<String> values = getAllValues(prefix); // 1 1
+        List<String> values = getAllValues(prefix);
         return values.isEmpty() ? Optional.empty() : Optional.of(values.get(values.size() - 1));
     }
 
