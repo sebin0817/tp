3213bec00d3bfabf116159e4c2fc755e0b6917f3
Rick <rick.kohjiaxuan@gmail.com>
2024-04-01 17:13:31 +0800

    Remove unused import in LogicManagerTest.java

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -15,7 +15,6 @@ import static seedu.address.testutil.TypicalPersons.AMY;
 
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
