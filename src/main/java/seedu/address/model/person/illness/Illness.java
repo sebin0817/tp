4d620de371a9e05a8e4f7a5fcb749879766001f0
jmsandiegoo <jmsandiego0710@gmail.com>
2024-04-01 13:59:56 +0800

    Fix Illness validation

--- src/main/java/seedu/address/model/person/illness/Illness.java
+++ src/main/java/seedu/address/model/person/illness/Illness.java
@@ -1,5 +1,10 @@
 package seedu.address.model.person.illness;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -8,8 +13,16 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Guarantees: immutable; name is valid as declared in {@link #isValidIllnessName(String)} (String)}
  */
 public class Illness {
-    public static final String MESSAGE_CONSTRAINTS = "Illness names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "^[A-Za-z0-9 ]+$";
+    public static final String MESSAGE_CONSTRAINTS = "Illness names should be of the following options \n"
+            + "(You could type just the first word / letters for convenience "
+            + "as long it corresponds to a single category.): \n"
+            + "Infectious Disease, Chronic Conditions, Autoimmune Disorders, Genetic Disorders, \n"
+            + "Mental Disorders, Neurological Disorders, Metabolic Disorder, Nutritional Deficiencies, \n"
+            + "Environmental Illnesses, Degenerative Diseases or Others.";
+    private static final Set<String> VALID_ILLNESS_CATEGORIES = new HashSet<>(Arrays.asList(
+            "Infectious Diseases", "Chronic Conditions", "Autoimmune Disorders", "Genetic Disorders",
+            "Mental Disorders", "Neurological Disorders", "Metabolic Disorder",
+            "Nutritional Deficiencies", "Environmental Illnesses", "Degenerative Diseases", "Others"));
     public final String illnessName;
 
     /**
@@ -19,15 +32,35 @@ public class Illness {
      */
     public Illness(String illnessName) {
         requireNonNull(illnessName);
+        String matchedIllness = findMatchingCategory(illnessName);
         checkArgument(isValidIllnessName(illnessName), MESSAGE_CONSTRAINTS);
-        this.illnessName = illnessName;
+        this.illnessName = matchedIllness;
     }
 
     /**
-     * Returns true if a given string is a valid illness name.
+     * Returns true if a given string is a valid illness.
      */
     public static boolean isValidIllnessName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return findMatchingCategory(test) != null;
+    }
+
+    /**
+     * Finds and returns the full illness category that partially and case-insensitively matches the input.
+     *
+     * @param illnessInput The input string to match.
+     * @return The full matched category name if match is found and is unique; null otherwise.
+     */
+    private static String findMatchingCategory(String illnessInput) {
+        String inputLowerCase = illnessInput.toLowerCase();
+        Set<String> matchedCategories = VALID_ILLNESS_CATEGORIES.stream()
+                .filter(category -> category.toLowerCase().startsWith(inputLowerCase))
+                .collect(Collectors.toSet());
+
+        if (matchedCategories.size() != 1) {
+            return null;
+        }
+
+        return matchedCategories.iterator().next();
     }
 
     @Override
